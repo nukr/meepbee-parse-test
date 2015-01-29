6f3ad26bc5deeaf7384b3e1f3d8a288fe3d5bbe5
Nukr <nukrs.w@gmail.com>
2015-01-29 18:35:29 +0800

    add routes

--- src/index.js
+++ src/index.js
@@ -10,50 +10,98 @@ let port = process.env.port || 5000;
 app.use(router(app));
 
 app.get('/', function*() {
-    this.body = 'hi';
+  this.body = 'hi';
 });
 
 app.get('/products', function*() {
-    let products =
-        yield meepbee.getObjects('Products');
+  let products =
+    yield meepbee.getObjects('Products');
 
-    this.body = products.body;
+  this.body = products.body;
 })
 
-app.get('/products/:productId', function *() {
-    let productId = this.params.productId;
-    let product = yield meepbee.getObject('Products', productId);
-    this.body = product.body;
+app.get('/products/:productId', function*() {
+  let productId = this.params.productId;
+  let product =
+    yield meepbee.getObject('Products', productId);
+  this.body = product.body;
 })
 
 app.get('/orders', function*() {
-    let orders =
-        yield meepbee.getObjects('Orders');
+  let orders =
+    yield meepbee.getObjects('Orders');
 
-    this.body = orders.body
+  this.body = orders.body
 })
 
-app.get('/orders/:orderId', function *() {
-    let orderId = this.params.orderId;
-    let order = yield meepbee.getObject('Orders', orderId);
-    this.body = order.body;
+app.get('/orders/:orderId', function*() {
+  let orderId = this.params.orderId;
+  let order =
+    yield meepbee.getObject('Orders', orderId);
+  this.body = order.body;
 })
 
-app.get('/login', function *(){
-  let user = yield meepbee.loginUser('nukr', 'akgeto0g');
+app.get('/login', function*() {
+  let user =
+    yield meepbee.loginUser('nukr', 'akgeto0g');
   this.body = user.body;
 });
 
-app.get('/users', function *() {
-  let users = yield meepbee.getUsers();
+app.get('/users', function*() {
+  let users =
+    yield meepbee.getUsers();
   this.body = users.body;
 })
 
-app.get('/comments', function *() {
-    let orders =
-        yield meepbee.getObjects('Comments');
-    this.body = orders.body
+app.get('/comments', function*() {
+  let orders =
+    yield meepbee.getObjects('Comments');
+  this.body = orders.body
 });
+
+app.post('/roles', function*() {
+  let roles =
+    yield meepbee.createRole({
+      "name": "tester",
+      "ACL": {
+        "*": {
+          "read": true
+        }
+      }
+    });
+  this.body = roles.body
+});
+
+app.get('/roles/:id', function *() {
+  let role =
+    yield meepbee.getRole(this.params.id)
+  this.body = role.body;
+});
+
+app.get('/roles', function*() {
+  let roles =
+    yield meepbee.getRoles();
+  this.body = roles.body;
+});
+
+app.delete('/roles/:id', function *() {
+  let role =
+    yield meepbee.deleteRole(this.params.id)
+  this.body = role.body;
+});
+
+app.put('/roles/:id', function *() {
+  let role =
+    yield meepbee.updateRole(this.params.id, {
+      "ACL": {
+        "*": {
+          "update": true
+        }
+      }
+    });
+  this.body = role.body;
+});
+
 
 app.listen(port);
 console.log('server start on port %d', port);
